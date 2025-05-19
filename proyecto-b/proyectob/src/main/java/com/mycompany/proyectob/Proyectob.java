46c31b8682b6f0b7e888cd45481145c3b87a2439
Alessander Acevedo <fabrizio1234x@gmail.com>
2025-05-18 23:58:02 -0500

    Agregado Proyecto B: Prueba

---
+++ proyecto-b/proyectob/src/main/java/com/mycompany/proyectob/Proyectob.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyectob;
+
+/**
+ *
+ * @author Fabrizio
+ */
+public class Proyectob {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
